88afad451823c3fc631926117a6147bb990d4a2f
tomosindberg <tomosindberg@users.noreply.github.com>
2015-05-05 22:35:45 -0700

    Delete lecture.js

--- day3/lecture.js
+++
@@ -1,3 +0,0 @@
-function yell(word, anotherWord, yetAnotherWord) { console.log(word && word.toUpperCase()); console.log(anotherWord && anotherWord.toUpperCase()); console.log(yetAnotherWord && yetAnotherWord.toUpperCase());
-}
-yell("one", "two", "three"); yell("one", "two");
